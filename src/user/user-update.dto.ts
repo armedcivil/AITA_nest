2c0aa1b618f2c4b20c01fe98f23f849842fd64bb
Mac User <laser_yellow@cnc.jp>
2024-03-20 07:21:02 +0000

    user の更新は渡されたあたいのみ対象とする

--- src/user/user-update.dto.ts
+++ src/user/user-update.dto.ts
@@ -1,23 +1,20 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { MinLength, IsEmail } from 'class-validator';
 import { IsConfirmed } from 'src/validators/is-confirmed.validator';
 import { Pair } from 'src/validators/pair.validator';
-import { Unique } from 'src/validators/unique.validator';
+import { UniqueIgnore } from 'src/validators/unique-ignore.validator';
 import { User } from './user.entity';
 
 export class UpdateUserDto {
-  @IsNotEmpty()
   name?: string;
 
-  @IsNotEmpty()
-  @Unique(User)
+  @IsEmail()
+  @UniqueIgnore(User)
   email?: string;
 
-  @IsNotEmpty()
   @MinLength(8)
   @Pair('passwordConfirmation')
   @IsConfirmed('passwordConfirmation')
   password?: string;
 
-  @IsNotEmpty()
   passwordConfirmation?: string;
 }
