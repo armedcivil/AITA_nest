b67618cdf4caa3079c9f408b8aa40422409d24b2
Mac User <laser_yellow@cnc.jp>
2024-06-07 07:18:51 +0000

    正常レスポンスに statusCode と message を追加

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { useContainer } from 'class-validator';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { validationExceptionFactory } from './common/exceptions/validation.exception';
+import { SuccessResponseInterceptor } from './success-response/success-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -25,8 +26,10 @@ async function bootstrap() {
       'http://localhost:4200',
       'http://localhost:4201',
       'http://localhost:17000',
+      'http://localhost:62905',
     ],
   });
+  app.useGlobalInterceptors(new SuccessResponseInterceptor());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.APP_PORT);
 }
